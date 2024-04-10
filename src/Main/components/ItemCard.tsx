5b96a79fc7ed4f4a0afe2d4b26cbcca9e9f595b7
poqop721 <poqop721@naver.com>
2024-04-10 18:03:24 +0900

    fix : reduced code redundanct (useEffect, search)

--- src/Main/components/ItemCard.tsx
+++ src/Main/components/ItemCard.tsx
@@ -1,6 +1,6 @@
-import { Products } from ".."
+import { Product } from ".."
 
-export default function ItemCard({item} : {item : Products}){
+export default function ItemCard({item} : {item : Product}){
     return(
         <ul>
             <img src={item.thumbnail} alt={item.title} />
--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,51 +1,68 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ItemCard from "./components/ItemCard";
 
-export interface Products{
+export interface Product{
     id : number,
     title : string,
-    description : string,
     price : number,
-    discountPercentage : number,
-    rating : number,
-    stock : number,
     brand : string,
-    category : string,
     thumbnail : string,
-    images: Array<string>
+    description? : string,
+    discountPercentage? : number,
+    rating? : number,
+    stock? : number,
+    category? : string,
+    images?: Array<string>
 }
 
 export default function Main() {
     const [inputText, setInputText] = useState<string>('')
-    const [products, setProducts] = useState<Products[]>([])
+    const [products, setProducts] = useState<Product[]>([])
+    const [limit, setLimit] = useState<number>(10)
+    const [keyword, setKeyword] = useState<string>('')
+    const moreButton = useRef<HTMLButtonElement>(null)
 
     useEffect(()=>{
-        fetch('https://dummyjson.com/products')
+        console.log(limit)
+        fetch(`https://dummyjson.com/products/search?q=${keyword}&limit=${limit}&select=id,thumbnail,title,brand,price`)
         .then(res => res.json())
-        .then(data => {setProducts(data.products)})
-    },[])
+        .then(data => {
+            setProducts(data.products)
+            if(moreButton.current){
+                if(data.total <= limit){
+                    moreButton.current.style.display = 'none'
+                } else {
+                    moreButton.current.style.display = 'block'
+                }
+            }
+        })
+    },[limit,keyword])
 
+    const search = (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setKeyword(inputText)
+        setLimit(10)
+    }   
     const onChangeInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputText(e.target.value);
       };
 
-    const search = async (e : React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        fetch(`https://dummyjson.com/products/search?q=${inputText}`)
-        .then(res => res.json())
-        .then(data => {setProducts(data.products)})
-    }   
+
+    const seeMore = () => {
+        setLimit(limit + 10)
+    }
 
     return (
         <div>
             <form
                 onSubmit={search}>
-                <input type="text" placeholder="상품 제목 입력" value={inputText} onChange={onChangeInputText}/>
+                <input type="text" placeholder="상품 제목 입력" value={inputText} onChange={onChangeInputText} required/>
                 <input type="submit" value="검색" />
             </form>
-            <div>{products.map((item : Products)=>(
+            <div>{products.map((item : Product)=>(
                 <ItemCard key={item.id} item={item} />
             ))}</div>
+            <button onClick={seeMore} ref={moreButton}>더보기</button>
         </div>
     )
 }
