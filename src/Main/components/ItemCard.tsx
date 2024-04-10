ff6183dc21a1d5c5fdbff01935215cddadbf31c2
poqop721 <poqop721@naver.com>
2024-04-10 20:08:01 +0900

    fix : fixed to 2 column

--- src/Main/components/ItemCard.tsx
+++ src/Main/components/ItemCard.tsx
@@ -1,11 +1,22 @@
+import styled from "styled-components"
 import { Product } from ".."
 
 export default function ItemCard({item} : {item : Product}){
+
+    const CardsItem = styled.li`
+    display: flex;
+    flex-direction : column;
+    border-radius: 10px;
+    border : 1px solid gray;
+    padding : 10px;
+    gap : 15px;
+    `
+
     return(
-        <ul>
+        <CardsItem>
             <img src={item.thumbnail} alt={item.title} />
             <li>{item.brand} - {item.title}</li>
             <li>{item.price}$</li>
-        </ul>
+        </CardsItem>
     )
 }
--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import ItemCard from "./components/ItemCard";
+import styled from "styled-components";
 
-export interface Product{
-    id : number,
-    title : string,
-    price : number,
-    brand : string,
-    thumbnail : string,
-    description? : string,
-    discountPercentage? : number,
-    rating? : number,
-    stock? : number,
-    category? : string,
+export interface Product {
+    id: number,
+    title: string,
+    price: number,
+    brand: string,
+    thumbnail: string,
+    description?: string,
+    discountPercentage?: number,
+    rating?: number,
+    stock?: number,
+    category?: string,
     images?: Array<string>
 }
 
@@ -22,45 +23,60 @@ export default function Main() {
     const [keyword, setKeyword] = useState<string>('')
     const moreButton = useRef<HTMLButtonElement>(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/search?q=${keyword}&limit=${limit}&select=id,thumbnail,title,brand,price`)
-        .then(res => res.json())
-        .then(data => {
-            setProducts(data.products)
-            if(moreButton.current){
-                if(data.total <= limit){
-                    moreButton.current.style.display = 'none'
-                } else {
-                    moreButton.current.style.display = 'block'
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data.products)
+                if (moreButton.current) {
+                    if (data.total <= limit) {
+                        moreButton.current.style.display = 'none'
+                    } else {
+                        moreButton.current.style.display = 'block'
+                    }
                 }
-            }
-        })
-    },[limit,keyword])
+            })
+            .catch(error => {
+                alert('상품을 불러오는데 문제가 발생했습니다.')
+                console.log(error)
+            })
+    }, [limit, keyword])
 
-    const search = (e : React.FormEvent<HTMLFormElement>) => {
+    const search = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setKeyword(inputText)
         setLimit(10)
-    }   
+    }
     const onChangeInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputText(e.target.value);
-      };
+    };
 
 
     const seeMore = () => {
         setLimit(limit + 10)
     }
 
+    const CardsWrapper = styled.div`
+        width: 90%;
+        padding: 4rem;
+        display : grid;
+        grid-template-columns : repeat(2,1fr);
+        gap : 2em 1.5em;
+        margin: 50px 0;
+    `
+
     return (
         <div>
             <form
                 onSubmit={search}>
-                <input type="text" placeholder="상품 제목 입력" value={inputText} onChange={onChangeInputText} required/>
+                <input type="text" placeholder="상품 제목 입력" value={inputText} onChange={onChangeInputText} required />
                 <input type="submit" value="검색" />
             </form>
-            <div>{products.map((item : Product)=>(
-                <ItemCard key={item.id} item={item} />
-            ))}</div>
+            <CardsWrapper>
+                {products.map((item: Product) => (
+                    <ItemCard key={item.id} item={item} />
+                ))}
+            </CardsWrapper>
             <button onClick={seeMore} ref={moreButton}>더보기</button>
         </div>
     )
