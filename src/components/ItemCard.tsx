6f38fbb9f94ebbca7ff667e4028a9a6ddaa0095c
poqop721 <poqop721@naver.com>
2024-04-12 22:00:20 +0900

    fix : fixed card css

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -4,15 +4,41 @@ import { CardsItem } from "../styleComponents/CardStyle"
 import { useNavigate } from "react-router-dom"
 import React from "react"
 
+const ImageDiv = styled.div`
+  width: 100%;
+  height: 250px;
+  background-position: center center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+  border-radius:10px;
+`
+
+const Image = styled.img`
+    opacity: 0;
+`
+
+const Price = styled.span`
+    font-size : 1.2em;
+    font-weight : 700;
+`
+
+const InfoDiv = styled.div`
+    padding : 0 0.7em;
+    line-height : 1.7em;
+`
 
 function ItemCard({item} : {item : Item}){
     const navigate = useNavigate()
 
     return(
         <CardsItem onClick={()=>{navigate(`/product/${item.id}`)}}>
-            <img src={item.thumbnail} alt={item.title} />
-            <p className="title">{item.brand} - {item.title}</p>
-            <p>{item.price}$</p>
+            <ImageDiv style={{backgroundImage:`url(${item.thumbnail})`}}>
+                <Image src={item.thumbnail} alt={item.title} />
+            </ImageDiv>
+            <InfoDiv>
+                <p className="title">{item.brand} - {item.title}</p>
+                <p><Price>{item.price}</Price>$</p>
+            </InfoDiv>
         </CardsItem>
     )
 }
