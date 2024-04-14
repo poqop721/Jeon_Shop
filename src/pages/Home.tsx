32bc21285c83de1bda433ac413ba55b2a50b8e3b
poqop721 <poqop721@naver.com>
2024-04-15 05:06:34 +0900

    fix : fixing scrolly not workin on vercel2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -46,8 +46,8 @@ function Home() {
                 } else {
                     setIsEnd(false)
                 }
-                window.scrollTo(0, scrollY)
                 setTotal(data.total)
+                window.scrollTo(0, scrollY)
             })
             .catch(error => {
                 alert('상품을 불러오는데 문제가 발생했습니다.')
